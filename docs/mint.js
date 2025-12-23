7e4de9d06ebb28208a2533766aa501accec2fbe2
9atetris <tetrisotoko777@gmail.com>
2025-12-24 07:25:23 +0900

    Add Prosperity Omamori mint page

---
+++ docs/mint.js
@@ -0,0 +1,10 @@
+const mintButton = document.getElementById('mint');
+const connectButton = document.getElementById('connect');
+
+connectButton.addEventListener('click', () => {
+  alert('Wallet connection will be wired here.');
+});
+
+mintButton.addEventListener('click', () => {
+  alert('Mint flow will be wired here.');
+});
